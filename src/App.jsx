9dbfdc11ff2409059ee53ea771688f1d0bf46cc8
Luka Cáceres <96329037+LukaCaceres@users.noreply.github.com>
2025-06-27 19:03:06 -0300

    Primer commit
    commit con inicializacion del frontend y algunos de sus modulos necesarios

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+    return (
+        <Router>
+            <header className='position-sticky top-0 z-3'>
+                
+            </header>
+            <main className="flex-grow-1">
+                <Routes>
+
+                </Routes>
+            </main>
+            <footer className='overflow-x-hidden'>
+                
+            </footer>
+        </Router>
+    )
+}
+
+export default App
