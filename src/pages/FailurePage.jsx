f0b45004cb744b272e0ec44bd3a0c2a199c6e059
Luka Cáceres <96329037+LukaCaceres@users.noreply.github.com>
2025-07-03 02:29:30 -0300

    vercel json
    se agrego el verceljson para vercel

--- src/pages/FailurePage.jsx
+++ src/pages/FailurePage.jsx
@@ -1,6 +1,6 @@
 const FailurePage = () => {
     return (
-        <div className="h-screen flex items-center justify-center flex-col">
+        <div className="bg-gray-900 h-screen flex items-center justify-center flex-col">
             <h1 className="text-2xl font-bold text-red-600 mb-4">Pago fallido</h1>
             <p className="text-lg text-gray-700">Hubo un problema con tu pago. Inténtalo nuevamente.</p>
         </div>
